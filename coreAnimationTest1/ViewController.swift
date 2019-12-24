a36fadbb2788a8e79e7e28a63d5ad8a258da9840
Amith's MacBook <aknkum@essex.ac.uk>
2019-12-24 16:42:42 +0000

    this is a commit which has a animation of a circle following on a track.

--- coreAnimationTest1/ViewController.swift
+++ coreAnimationTest1/ViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let shapeLayer = CAShapeLayer()
+    let tracLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        let center = view.center
+        
+        
+        
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2 , endAngle: 2 * CGFloat.pi, clockwise: true)
+        tracLayer.path = circularPath.cgPath
+        
+        tracLayer.fillColor = UIColor.clear.cgColor
+        tracLayer.strokeColor = UIColor(red: 230/255, green: 221/255, blue: 221/255, alpha: 1.0).cgColor
+        tracLayer.lineWidth = 10
+        tracLayer.lineCap = .round
+        
+        shapeLayer.strokeEnd = 0
+        
+
+        shapeLayer.path = circularPath.cgPath
+        
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.systemPink.cgColor
+        shapeLayer.lineWidth = 10
+        shapeLayer.lineCap = .round
+        
+        shapeLayer.strokeEnd = 0
+        
+        view.layer.addSublayer(tracLayer)
+        view.layer.addSublayer(shapeLayer)
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(screenTap)))
     }
 
+    @objc func screenTap() {
+        print("tapping")
+        
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.byValue  = 1
+        basicAnimation.duration = 3
+        
+        basicAnimation.fillMode = .forwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnimation, forKey: "")
+        print("end taping")
+    }
 
 }
 
